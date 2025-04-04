5b713b7beb9d42f9f85d17df10f8d3ab5ac1575e
Juris Būmanis <juris.bumanis@pata.lv>
2025-04-04 16:24:39 +0300

    added with real data

---
+++ resources/js/Components/CountryList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-list>
+    <ion-list-header>{{ headerTitle }}</ion-list-header>
+    <ion-item
+        v-for="country in countries"
+        :key="country.id"
+        :href="`/country/${country.id}`">
+      <ion-label>{{ country.common_name }}</ion-label>
+    </ion-item>
+  </ion-list>
+</template>
+
+<script>
+export default {
+  name: 'CountryList',
+  props: {
+    headerTitle: {
+      type: String,
+      default: 'Countries'
+    },
+    countries: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
--- resources/js/Pages/Country/Index.vue
+++ resources/js/Pages/Country/Index.vue
@@ -2,7 +2,7 @@
   <AppLayout title="Countrypedia Home">
     <div class="container mx-auto p-4">
       <SearchInput @update-query="handleSearch" />
-
+      <CountryList header-title="Search Results" :countries="searchResults" />
       <FavoriteCountries :favorites="favoriteCountries" />
     </div>
   </AppLayout>
@@ -12,23 +12,42 @@
 import AppLayout from '../../Layouts/AppLayout.vue';
 import SearchInput from '../../Components/SearchInput.vue';
 import FavoriteCountries from '../../Components/FavoriteCountries.vue';
+import CountryList from '../../Components/CountryList.vue';
 
 export default {
   name: 'Home',
   components: {
     AppLayout,
     SearchInput,
-    FavoriteCountries
+    FavoriteCountries,
+    CountryList
   },
   props: {
+    countries: {
+      type: Array,
+      default: () => []
+    },
     favoriteCountries: {
       type: Array,
       default: () => []
     }
   },
+  data() {
+    return {
+      searchResults: []
+    };
+  },
   methods: {
     handleSearch(query) {
       console.log('Searching for:', query);
+      if (query.trim() !== '') {
+        // Simulate search by filtering the countries prop
+        this.searchResults = this.countries.filter(country =>
+            country.common_name.toLowerCase().includes(query.toLowerCase())
+        );
+      } else {
+        this.searchResults = [];
+      }
     }
   }
 };

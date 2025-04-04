32ca63a9dbd37f4d586cccb539f131b870805988
Juris Būmanis <juris.bumanis@pata.lv>
2025-04-04 16:23:45 +0300

    cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,7 +24,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${appName}`,
     resolve: (name) =>
         resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({el, App, props, plugin}) {

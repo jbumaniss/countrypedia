6f423b4311583a0f34acd00f4de268fd9f7f5e15
Juris Būmanis <juris.bumanis@pata.lv>
2025-04-04 16:24:54 +0300

    service method added

--- src/Domain/Country/Services/CountryService.php
+++ src/Domain/Country/Services/CountryService.php
@@ -3,11 +3,21 @@
 namespace Domain\Country\Services;
 
 use Domain\Country\Models\Country;
+use Illuminate\Support\Collection;
 
 class CountryService
 {
-    public function list(): array
+    /**
+     * @return Collection<int, Country>
+     */
+    public function list(): Collection
     {
-        return Country::query()->list();
+        return Country::query()->list()->get();
+    }
+
+    public function show(int $id): Country
+    {
+        return Country::query()
+            ->find($id);
     }
 }

8636cbdd557733b43ed185c65b7c0230e855c6bf
Juris Būmanis <juris.bumanis@pata.lv>
2025-04-05 11:51:14 +0300

    fixing issue with duplicate

--- database/migrations/2025_04_03_182738_create_country_aliases_table.php
+++ database/migrations/2025_04_03_182738_create_country_aliases_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('country_aliases', function (Blueprint $table) {
             $table->id();
 
-            $table->string('code')->unique();
+            $table->string('code');
             $table->string('official');
             $table->string('common');
 

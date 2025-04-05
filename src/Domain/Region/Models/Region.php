cad763ccd6f47adf55cc591bc0ac326eb0fb7d6d
Juris Būmanis <juris.bumanis@pata.lv>
2025-04-05 15:36:40 +0300

    types added

--- src/Domain/Region/Models/Region.php
+++ src/Domain/Region/Models/Region.php
@@ -4,6 +4,10 @@ namespace Domain\Region\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ */
 class Region extends Model
 {
     protected $guarded = [];

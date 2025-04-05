60e51068f7220f875ffe59e1df6a3a2dca45ce8a
Juris Būmanis <juris.bumanis@pata.lv>
2025-04-05 15:36:10 +0300

    subgroup model created

---
+++ src/Domain/Region/Models/SubRegion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Domain\Region\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ */
+class SubRegion extends Model
+{
+    protected $guarded = [];
+
+}
